116ae0a8b2f5cbe67d1ec6d0f0cbc28cb4b1a2de
Omar Hernandez <happones@labbe.io>
2020-12-02 19:45:29 -0600

    Fix nativesclass ios

--- publish/pack.sh
+++ publish/pack.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SOURCE_DIR=../src;
-TO_SOURCE_DIR=src;
-PACK_DIR=package;
-ROOT_DIR=..;
+SOURCE_DIR=../src
+TO_SOURCE_DIR=src
+PACK_DIR=package
+ROOT_DIR=..
 PUBLISH=--publish
 
-install(){
+install() {
     npm i
 }
 
@@ -22,7 +22,7 @@ pack() {
 
     # copy README & LICENSE to src
     echo 'Copying README and LICENSE to /src...'
-    node_modules/.bin/ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE
+    node_modules/.bin/ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE.md
     node_modules/.bin/ncp "$ROOT_DIR"/README.md "$TO_SOURCE_DIR"/README.md
 
     # compile package and copy files required by npm
